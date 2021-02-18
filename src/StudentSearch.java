a298a831cd600c8b963266fdaf7b4c4531e33873
Elgun Jabrayilzade <elgun1999@gmail.com>
2021-02-18 19:16:36 +0300

    throw exception for empty list

--- src/StudentSearch.java
+++ src/StudentSearch.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
 
 public class StudentSearch {
-    public boolean studentExists(ArrayList<Student> students, String id) {
+    public boolean studentExists(ArrayList<Student> students, String id) throws Exception {
+        if (students.isEmpty()) {
+            throw new Exception("Students list should not be empty!");
+        }
         for (Student student: students)
             if (student.getId().equals(id))
                 return true;
