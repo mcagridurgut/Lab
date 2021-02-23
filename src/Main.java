5ea4656526ecf8ead21e50bf6264a69f568a7678
mcagri <mcagridurgut@gmail.com>
2021-02-23 16:20:58 +0300

    add an example execution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        
-    }
+public static void main(String[] args) {
+Student student1 = new Student("student1", "2222", "st1@gmail.com");
+StudentSearch search = new StudentSearch();
+ArrayList<Student> students = new ArrayList<Student>();
+students.add(student1);
+try {
+System.out.println(search.findOne(students, "student1").getId());
+} catch (Exception e) {
+System.out.println(e.getMessage());
+}
 }
