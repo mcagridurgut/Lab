30dfd2b5eb54d14020c546dc29cd14e54726fc07
mcagri <mcagridurgut@gmail.com>
2021-02-23 15:58:23 +0300

    implemented findAll method

--- src/StudentSearch.java
+++ src/StudentSearch.java
@@ -18,5 +18,17 @@ public class StudentSearch {
 				return student;
 		throw new Exception("There is no student with the given name!");
 	}
+	public ArrayList<Student> findAll(ArrayList<Student> students, String name) throws
+Exception {
+ArrayList<Student> result = new ArrayList<Student>();
+for (Student student: students)
+if (student.getName().equals(name)) {
+result.add(student);
+}
+if (result.isEmpty()) {
+throw new Exception("There is no student with the given name!");
+}
+return result;
+}
 
 }
